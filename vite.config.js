de92d8bc1fc3237f1d440c8d9a9d2b9401a705b5
rajathshttgr <rajathshettigar.uy@gmail.com>
2024-11-10 10:51:24 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 
 export default defineConfig({
-  base: '/netflix-clone/', // Replace with your repository name
+  //base: '/netflix-clone/', // Replace with your repository name
   plugins: [react()],
 });
